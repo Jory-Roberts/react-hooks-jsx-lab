829b3899425737b57542e8217e1978381a6d5a8d
Jory Roberts <robertsjory13@gmail.com>
2023-05-16 18:59:34 -0500

    completes lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
-import React from "react";
-import { name, city } from "../data/data.js";
+import React from 'react';
+import { name, city } from '../data/data.js';
+
+const styles = {
+    color: 'firebrick',
+};
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+    // update the JSX being returned!
+    return (
+        <div id='home'>
+            Home
+            <h1 style={styles}>
+                {name} is a Web Developer from {city}
+            </h1>
+        </div>
+    );
 }
 
 export default Home;
